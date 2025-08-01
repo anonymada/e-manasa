6c8aafccd418e998a7d674e404cfa0d7c4461ca3
{Jacky HERINAVALONA} <{votre_email}>
2025-08-01 10:13:09 +0300

    fix pages not rendering correctly

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/anonymada/e-manasa.git",
+  base: "https://anonymada.github.io/e-manasa/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
